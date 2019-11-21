0f06d885ea20b3a86c4291fb997a15e87b2f4e4e
Emil Østergaard <isdragon112@gmail.com>
2019-11-21 14:02:37 +0100

    Changed some more config and added some images to the file structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,40 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import ImageContainer from '../components/image'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const { childImageSharp } = data.file
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <ImageContainer fluid={childImageSharp.fluid} />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query HomePageQuery {
+    file(relativePath: { eq: "hero-image.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1680) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+IndexPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
 
 export default IndexPage
